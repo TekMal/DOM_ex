c51be55397d0f1bb4a5f314b3c3fa7665440d938
Kamila Kochanowska <k.kochanowska6@gmail.com>
2019-06-07 13:22:54 +0200

    added event click for operators without = and .

--- kalkulator/script.js
+++ kalkulator/script.js
@@ -16,8 +16,19 @@ numbers = buttons.filter((button) => {
 })
 console.log(numbers)
 
+function addElement(element, text, target) {
+    let htmlElement = document.createElement(element)
+    htmlElement.innerText = text
+    target.after(htmlElement)
+    // target.after(htmlElement)
+    // htmlElement.before(target)
+    return htmlElement
+  }
 
-// Create AC button
+let acButton = addElement("button", "AC", display)
+
+
+/*// Create AC button
 let acButton = document.createElement('button')
 acButton.innerText = 'AC'
 
@@ -26,7 +37,7 @@ displayParent.append(acButton)
 
 //Create dot
 
-/*let dotButton = document.createElement("button")
+let dotButton = document.createElement("button")
 dotButton.innerText = "."
 
 let divs = Array.from(document.firstElementChild.children[1].children)
@@ -43,4 +54,31 @@ numbers.forEach((button) => {
         const number = event.target.innerText
         display.value = display.value + number
     })
-});
+})
+
+acButton.addEventListener("click", (event) => {
+    display.value = ""
+})
+
+operators.forEach((operator) => {
+    operator.addEventListener("click", (event) => {
+        const operatorButton = event.target.innerText
+        const displayValue = display.value
+        const arrayOfOperators = ["+", "-", "/", "*"]
+        
+        if(operatorButton === "="){
+            display.value = eval(display.value)
+        }
+
+
+        const hasOperator = arrayOfOperators.some(opr => {
+           return displayValue.includes(opr)
+        })
+        if (hasOperator){
+            return
+        }
+        else{
+            display.value = display.value + " " + operatorButton + " "
+        }
+    })
+})
