cd33a2ba17f288fe677311baf1e4c3586e6a43b8
Kamila Kochanowska <k.kochanowska6@gmail.com>
2019-06-07 12:11:46 +0200

    added event click for numbers

--- kalkulator/script.js
+++ kalkulator/script.js
@@ -4,9 +4,9 @@ display.disabled = true
 let operators = document.getElementsByClassName("operator")
 operators = Array.from(operators)
 
-let numbers = document.getElementsByTagName("button")
-numbers = Array.from(numbers).filter((value) => {
-    let number = Number(value.textContent)
+let buttons = Array.from(document.getElementsByTagName("button"))
+numbers = buttons.filter((button) => {
+    let number = Number(button.textContent)
     if (!isNaN(number)){
         return true
     }
@@ -15,3 +15,32 @@ numbers = Array.from(numbers).filter((value) => {
     }
 })
 console.log(numbers)
+
+
+// Create AC button
+let acButton = document.createElement('button')
+acButton.innerText = 'AC'
+
+let displayParent = display.parentElement
+displayParent.append(acButton)
+
+//Create dot
+
+/*let dotButton = document.createElement("button")
+dotButton.innerText = "."
+
+let divs = Array.from(document.firstElementChild.children[1].children)
+  .filter(element => element.tagName === 'DIV')
+
+let lastRowButtons = divs[4]
+let zeroButton = lastRowButtons.firstElementChild
+let plusButton = zeroButton.nextElementSibling
+lastRowButtons.insertBefore(dotButton, plusButton)
+*/
+
+numbers.forEach((button) => {
+    button.addEventListener("click", (event) => {
+        const number = event.target.innerText
+        display.value = display.value + number
+    })
+});
